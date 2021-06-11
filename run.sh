0c1ea8fe1c2c872084f7d24de91c6e1c1df9ac4f
EdmundGoodman <egoodman3141@gmail.com>
2021-06-11 19:17:16 +0100

    Modified script to display what it's doing, and do some very basic installs as well (e.g. we need zsh to use half the stuff etc.)

--- run.sh
+++ run.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-echo "Started copying dot files"
-cp -a  ./files/. ~/
-
 echo "Started necessary installs"
+for i in $(cat necessaryInstalls.txt); do
+    echo "    $i"
+    sudo apt install $i
+done
 
+echo "Started copying dot files"
+for i in $(ls -A ./files/); do
+    echo "    $i"
+    cp ./files/$i ~/$i
+done
 
+echo "Done"
